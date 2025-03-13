2aa55c6f27e6a8b4e9d38b743712903d26e1db16
Salvatore Pizzuto <spizzuto@xeniaprogetti.it>
2025-03-13 15:06:47 +0100

    scheletro generale e bozza di funzione colore

---
+++ script.ts
@@ -0,0 +1,45 @@
+function coloreInEsadecimale(nomeColore: string): string {
+    const colori: { [key: string]: string } = {
+        "black": "#000000",
+        "white": "#FFFFFF",
+        "red": "#FF0000",
+        "green": "#008000",
+        "blue": "#0000FF",
+        "yellow": "#FFFF00",
+        "cyan": "#00FFFF",
+        "magenta": "#FF00FF",
+        "gray": "#808080",
+        "lightgray": "#D3D3D3",
+        "darkgray": "#A9A9A9",
+        "pink": "#FFC0CB",
+        "brown": "#A52A2A",
+        "orange": "#FFA500",
+        "purple": "#800080",
+        "lime": "#00FF00",
+        "aqua": "#00FFFF",
+        "teal": "#008080",
+        "navy": "#000080"
+    };
+
+    const colore = nomeColore.toLowerCase();
+
+    if (colori[colore]) {
+        return colori[colore];
+    } else {
+        return "Colore non trovato!";
+    }
+}
+
+function selectColor(){
+    let color1 = document.querySelector(".choice")?.innerHTML;
+    // document.querySelector(".dropbtn")
+    alert("colori")
+}
+
+document.querySelector('.choice')?.addEventListener('click', (event) => {
+    event.preventDefault;
+    const color = event.target as HTMLButtonElement;
+    
+    const buttonText = color.innerText;
+    alert(color.innerText); 
+});
